a0bb51fb4af58e979d0bdc60634ae47611e54a4e
SDMasterGames <sdmastergames@hotmail.com>
2022-05-10 06:45:53 -0400

    alterando o nome da função list

--- src/modules/User/listUser/listUser.test.ts
+++ src/modules/User/listUser/listUser.test.ts
@@ -2,18 +2,19 @@ import { listUser } from "./listUser";
 const createSpy = jest.fn();
 const listSpy = jest.fn();
 const deleteByIdSpy = jest.fn();
-const findByIdSpy = jest.fn()
-
+const findByIdSpy = jest.fn();
+const findByIdAndUpdateSpy = jest.fn();
 const suit = new listUser({
   create: createSpy,
-  List: listSpy,
+  list: listSpy,
   deleteById: deleteByIdSpy,
   findById: findByIdSpy,
+  findByIdAndUpdate: findByIdAndUpdateSpy,
 });
 
-describe("listUser",()=>{
-    it("Listando usuarios com sucesso.", async () => {
-        await expect(suit.execute()).resolves.not.toThrow();
-        expect(listSpy).toHaveBeenCalled();
-    })
+describe("listUser", () => {
+  it("Listando usuarios com sucesso.", async () => {
+    await expect(suit.execute()).resolves.not.toThrow();
+    expect(listSpy).toHaveBeenCalled();
+  });
 });
--- src/modules/User/listUser/listUser.ts
+++ src/modules/User/listUser/listUser.ts
@@ -3,7 +3,7 @@ import { IUserRepo } from "../../../repository/IUserRepo";
 export class listUser {
   constructor(private IUserRepo: IUserRepo) {}
   async execute() {
-    const users = await this.IUserRepo.List();
+    const users = await this.IUserRepo.list();
     return users;
   }
 }
